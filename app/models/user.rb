1bb0926122f25a68f00b7c8f0bf9439a415d6b8f
JeongHo <wwwkang8@gmail.com>
2018-12-26 03:26:42 +0900

    Add association between user and articles and add debugger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_many :articles
+  before_save {self.email.downcase} #이메일이 저장되기 전에 소문자로 변환하는 것. 그리고 DB에 저장
   validates :username, presence: true, uniqueness: {case_sensitive: false},
             length: {minimum:3, maximum:25}
 
@@ -6,4 +9,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: { with: VALID_EMAIL_REGEX }
+
 end
