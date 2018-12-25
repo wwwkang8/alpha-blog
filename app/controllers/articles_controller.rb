34f53aa428c952cfe1df83b01729619f731b60d0
JeongHo <wwwkang8@gmail.com>
2018-12-25 22:25:38 +0900

    extract redundany and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     # 데이터베이스에서 모든 article을 가져온다.
@@ -20,16 +21,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def edit
     #특정 article을 가져오는 것
-    @article=Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article) #article show page
@@ -39,15 +40,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
 end
