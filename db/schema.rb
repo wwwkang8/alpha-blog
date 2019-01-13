861c943fb0ed171f5eb119b400ad0a58922bc2df
강정호 <jeongho.kang@dramancompany.com>
2019-01-13 12:34:04 +0900

    관리자 사용자 삭제 기능 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_010429) do
+ActiveRecord::Schema.define(version: 2019_01_13_030932) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_12_28_010429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
